676ee8259f1a8e1c0ddc238fcec789f372e987fc
Paguiar735 <72931357+Paguiar735@users.noreply.github.com>
2023-05-24 18:22:39 -0400

    [FEAT]: add splash screen

--- UC11/SA1_AT1a4/app/src/main/java/com/example/uc11/MainActivity.java
+++ UC11/SA1_AT1a4/app/src/main/java/com/example/uc11/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.uc11;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SplashScreen.installSplashScreen(this);
         setContentView(R.layout.activity_main);
     }
 }
