4926d418f714e6a2b971548bb07c3ab362d5f671
Paguiar735 <72931357+Paguiar735@users.noreply.github.com>
2023-06-04 20:23:58 -0400

    [FEAT]: add bottom nav bar

--- UC11/SA1_AT1a4/app/src/main/java/com/example/uc11/MainActivity.java
+++ UC11/SA1_AT1a4/app/src/main/java/com/example/uc11/MainActivity.java
@@ -2,15 +2,39 @@ package com.example.uc11;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.uc11.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SplashScreen.installSplashScreen(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new AddItemsFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.add_items)
+                replaceFragment(new AddItemsFragment());
+            else if (item.getItemId() == R.id.list_items)
+                replaceFragment(new ListItemsFragment());
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
